afd82b1c7363fa47cc8ca8f8da9e5bd856b5b833
Mathias Tranzer <mathias.tranzer@free.fr>
2023-04-07 20:15:48 +0200

    add mongoDb

--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,7 @@ const app = require('./app');
 
 const {loadPlanetsData} = require('./models/planets.model');
 const {loadLaunchesData} = require('./models/launches.model');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
